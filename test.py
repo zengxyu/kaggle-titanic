771e1cc466dd6d1009c164913933578cab92309f
zeng xiangyu <sduzengxiangyu@outlook.com>
2020-04-21 20:54:06 +0200

    delete test

--- test.py
+++
@@ -1,9 +0,0 @@
-import tkinter
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-import seaborn as sns
-matplotlib.use('TkAgg')
-x = np.random.normal(size=100)
-sns.distplot(x)
-plt.show()
